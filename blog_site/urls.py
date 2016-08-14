e76ed4889d5bcd99031d98b04aba35083aa80c08
PatrickCmd <pwalukagga@gmail.com>
2016-08-14 14:06:07 +0300

    modified blog/urls and post_list to render correctly.

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls', namespace='django-posts')),
+    url(r'', include('blog.urls')),
 ]
